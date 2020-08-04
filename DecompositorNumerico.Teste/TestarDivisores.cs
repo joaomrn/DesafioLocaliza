3002162cd3168628c2a0adb09406f2115e05ef0a
João Vitor <joaovitormrn@gmail.com>
2020-08-04 11:55:45 -0300

    Adicionado metodo de teste para calculara resposta.

---
+++ DecompositorNumerico.Teste/TestarDivisores.cs
@@ -0,0 +1,28 @@
+﻿using DecompositorNumerico.Dominio;
+using DecompositorNumerico.Servico;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecompositorNumerico.Teste
+{
+    [TestClass]
+    public class TestarDivisores
+    {
+        [TestMethod]
+        public void TestarDivisoresEPrimos()
+        {
+            int sucesso = 0;
+            try
+            {
+                Divisores divisores = new Divisores();
+                Divisor resposta = divisores.Calcular(40);
+                sucesso = 1;
+            }
+            catch (System.Exception)
+            {
+                sucesso = 0;
+            }
+
+            Assert.AreEqual(sucesso, 1);
+        }
+    }
+}
